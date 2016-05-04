99aa3ba3cc8be7067ddfb665c0c6e4a2c4ab113c
awbasham <awbasham@purdue.edu>
2016-05-04 12:17:14 -0400

    Removed unused integer

--- src/Levels/Level1.java
+++ src/Levels/Level1.java
@@ -9,10 +9,8 @@ public class Level1 {
 
     public TileSet load() {
         tileSet = new TileSet(level.length, level[0].length);
-        int id;
         for (int i = 0; i < level.length; i++) {
             for (int j = 0; j < level[0].length; j++) {
-                id = level[i][j];
                 tileSet.addTile(new Tile(level[i][j], i * 64, j * 64), i, j);
             }
         }
